eefa0b57c1b870d64b38848348b57c2b735c18a2
Fabio <fabio@fabiofarias.com.br>
2020-07-15 20:28:23 -0300

    Lock yarn

--- src/server.js
+++ src/server.js
@@ -4,10 +4,11 @@ const path = require('path');
 const resolvers = require('./resolvers');
 const mongoose = require('mongoose');
 
-let url = "mongodb://localhost:27017/graphqlnode?retryWrites=true&w=majority";
+// let url = "mongodb://localhost:27017/admin?retryWrites=true&w=majority";
+let url = "mongodb+srv://cluster0.3avtk.mongodb.net/Cluster0?retryWrites=true&w=majority";
 mongoose.connect(url, {
-  user:'root',
-  pass:'example',
+  user:'teste',
+  pass:'7uROi25QIvAuSv9T',
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).catch(erro => {
